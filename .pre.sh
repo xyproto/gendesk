963be7a5eee619bc28e0d24d8bbe84fa11f17869
Alexander F Rødseth <rodseth@gmail.com>
2018-01-16 13:42:01 +0100

    Release binaries in their own diretories

--- .pre.sh
+++ .pre.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-awk '/Change/{flag=1}/General information/{flag=0}flag' README.md > ChangeLog.md
+# Generate a changelog from the entries in the readme
+awk '/Change/{flag=1}/General information/{flag=0}flag' README.md > CHANGELOG.md
+# Download the default icon
 curl -s -O 'http://roboticoverlords.org/images/default.png'
