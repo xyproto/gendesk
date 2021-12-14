bb288e270d1817dbf455fca8bfca13cd70211db5
Alexander F. Rødseth <rodseth@gmail.com>
2021-12-14 10:20:26 +0100

    Add version script

---
+++ version.sh
@@ -0,0 +1,27 @@
+#!/bin/sh -e
+#
+# Self-modifying script that updates the version numbers
+# Requires "setconf"
+#
+
+# The current version goes here, as the default value
+VERSION=${1:-'1.0.9'}
+
+if [ -z "$1" ]; then
+  echo "The current version is $VERSION, pass the new version as the first argument if you wish to change it"
+  exit 0
+fi
+
+echo "Setting the version to $VERSION"
+
+# Set the version in various files
+setconf README.md '* Version' $VERSION
+setconf main.go versionString "\"Desktop File Generator "$VERSION"\""
+
+# Update the date and version in the man page
+d=$(LC_ALL=C date +'%d %b %Y')
+sed -i "s/\"[0-9]* [A-Z][a-z]* [0-9]*\"/\"$d\"/g" gendesk.1
+sed -i "s/[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+/$VERSION/g" gendesk.1
+
+# Update the version in this script
+sed -i "s/[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+/$VERSION/g" "$0"
