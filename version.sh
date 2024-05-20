d6f2b0d6dad1a5ac20b7319fd1a586cea95452cb
Alexander F. Rødseth <rodseth@gmail.com>
2024-05-20 20:16:34 +0200

    Update the version number in version.sh

--- version.sh
+++ version.sh
@@ -5,7 +5,7 @@
 #
 
 # The current version goes here, as the default value
-VERSION=${1:-'1.0.9'}
+VERSION=${1:-'1.0.10'}
 
 if [ -z "$1" ]; then
   echo "The current version is $VERSION, pass the new version as the first argument if you wish to change it"
